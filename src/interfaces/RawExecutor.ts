73afc3effc1f0cba09b562b8eb602d5b500b7d9f
Tiago <venceslau.tiago@gmail.com>
2024-10-01 16:52:17 +0100

    refs #1 - initial commit

--- src/interfaces/RawExecutor.ts
+++ src/interfaces/RawExecutor.ts
@@ -6,14 +6,14 @@
  * @category Query
  */
 export interface RawExecutor<T> {
-    /**
-     * @summary Executes a raw instruction in the Database
-     *
-     * @typeDef V the expected outcome of the instruction
-     * @param rawInput
-     * @param args
-     *
-     * @method
-     */
-    raw<V>(rawInput: T, ...args: any[]): Promise<V>
+  /**
+   * @summary Executes a raw instruction in the Database
+   *
+   * @typeDef V the expected outcome of the instruction
+   * @param rawInput
+   * @param args
+   *
+   * @method
+   */
+  raw<V>(rawInput: T, ...args: any[]): Promise<V>;
 }

1e0e1a614163ef3624c9a2c49566d80b25e5534d
Tiago <venceslau.tiago@gmail.com>
2024-11-07 01:20:25 +0000

    refs #1 - update

--- src/model/BaseModel.ts
+++ src/model/BaseModel.ts
@@ -1,6 +1,5 @@
 import { DBOperations, timestamp } from "@decaf-ts/db-decorators";
 import { ModelArg, Model } from "@decaf-ts/decorator-validation";
-import { createdBy, updatedBy } from "./decorators";
 
 export abstract class BaseModel extends Model {
   @timestamp(DBOperations.CREATE)

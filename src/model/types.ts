006e1980be1b3b1d47c426b9307624f6f3683034
Tiago Venceslau <59795506+TiagoVenceslau@users.noreply.github.com>
2025-09-05 12:24:45 +0100

    DECAF-171 - Add support for join table options to relationship decorators (#27)

--- src/model/types.ts
+++ src/model/types.ts
@@ -1,7 +1,55 @@
 import { CascadeMetadata } from "../repository";
 import { Constructor } from "@decaf-ts/decorator-validation";
 
-export type JoinTableOptions = Record<string, any>;
+/**
+ * Describes join column options.
+ */
+export type JoinColumnOptions = {
+  /**
+   * Name of the column.
+   */
+  name?: string;
+  /**
+   * Name of the column in the entity to which this column is referenced.
+   */
+  referencedColumnName?: string;
+  /**
+   * Name of the foreign key constraint.
+   */
+  foreignKeyConstraintName?: string;
+};
+
+export type JoinTableOptions = {
+  /**
+   * Name of the table that will be created to store values of the both tables (join table).
+   * By default is auto generated.
+   */
+  name?: string;
+  /**
+   * First column of the join table.
+   */
+  joinColumn?: JoinColumnOptions;
+  /**
+   * Second (inverse) column of the join table.
+   */
+  inverseJoinColumn?: JoinColumnOptions;
+};
+
+export type JoinTableMultipleColumnsOptions = {
+  /**
+   * Name of the table that will be created to store values of the both tables (join table).
+   * By default is auto generated.
+   */
+  name?: string;
+  /**
+   * First column of the join table.
+   */
+  joinColumns?: JoinColumnOptions[];
+  /**
+   * Second (inverse) column of the join table.
+   */
+  inverseJoinColumns?: JoinColumnOptions[];
+};
 
 /**
  * @description Metadata for model relationships

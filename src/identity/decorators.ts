130dc5e81e96647983507ed30de6db691bf802f0
tsoxos <giannis.tsochos@pdmfc.com>
2025-12-08 12:13:03 +0200

    DECAF-178 reinsert code for default generated in pk decorator

--- src/identity/decorators.ts
+++ src/identity/decorators.ts
@@ -184,7 +184,12 @@ export function pk(
     "cycle" | "startWith" | "incrementBy"
   > = DefaultSequenceOptions
 ) {
-  opts = Object.assign({}, DefaultSequenceOptions, opts) as SequenceOptions;
+      opts = Object.assign({}, DefaultSequenceOptions, opts, {
+    generated:
+      opts.type && typeof opts.generated === "undefined"
+        ? true
+        : opts.generated || DefaultSequenceOptions.generated,
+  }) as SequenceOptions;
   return Decoration.for(DBKeys.ID)
     .define({
       decorator: pkDec,

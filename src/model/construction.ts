b5ef150d6d088761c6c9467ab7361896448f3b75
tvenceslau <venceslau.tiago@gmail.com>
2025-12-22 01:35:09 +0000

    patch - DECAF-206 relation handlers update-no-ci - after release preparation

--- src/model/construction.ts
+++ src/model/construction.ts
@@ -175,7 +175,7 @@ export async function oneToOneOnCreate<M extends Model, R extends Repo<M>>(
   if (!constructor)
     throw new InternalError(`Could not find model ${data.class}`);
   const repo: Repo<any> = Repository.forModel(constructor, this.adapter.alias);
-  const created = await repo.create(propertyValue);
+  const created = await repo.create(propertyValue, context);
   const pk = Model.pk(created);
   await cacheModelForPopulate(context, model, key, created[pk], created);
   (model as any)[key] = created[pk];
@@ -245,7 +245,7 @@ export async function oneToOneOnUpdate<M extends Model, R extends Repo<M>>(
       key,
       this.adapter.alias
     );
-    const read = await innerRepo.read(propertyValue);
+    const read = await innerRepo.read(propertyValue, context);
     await cacheModelForPopulate(context, model, key, propertyValue, read);
     (model as any)[key] = propertyValue;
     return;
@@ -325,10 +325,11 @@ export async function oneToOneOnDelete<M extends Model, R extends Repo<M>>(
   );
   let deleted: M;
   if (!(propertyValue instanceof Model))
-    deleted = await innerRepo.delete(model[key] as string);
+    deleted = await innerRepo.delete(model[key] as string, context);
   else
     deleted = await innerRepo.delete(
-      (model[key] as M)[innerRepo.pk as keyof M] as string
+      (model[key] as M)[innerRepo.pk as keyof M] as string,
+      context
     );
   await cacheModelForPopulate(
     context,
@@ -411,7 +412,7 @@ export async function oneToManyOnCreate<M extends Model, R extends Repo<M>>(
   if (arrayType !== "object") {
     const repo = repositoryFromTypeMetadata(model, key, this.adapter.alias);
     for (const id of uniqueValues) {
-      const read = await repo.read(id);
+      const read = await repo.read(id, context);
       await cacheModelForPopulate(context, model, key, id, read);
     }
     (model as any)[key] = [...uniqueValues];
@@ -556,7 +557,7 @@ export async function oneToManyOnDelete<M extends Model, R extends Repo<M>>(
   ]);
 
   for (const id of uniqueValues.values()) {
-    const deleted = await repo.delete(id);
+    const deleted = await repo.delete(id, context);
     await cacheModelForPopulate(context, model, key, id, deleted);
   }
   (model as any)[key] = [...uniqueValues];
@@ -700,7 +701,7 @@ export async function populate<M extends Model, R extends Repo<M>>(
           alias
         );
         if (!repo) throw new InternalError("Could not find repo");
-        val = await repo.read(proKeyValue);
+        val = await repo.read(proKeyValue, context);
       }
       results.push(val);
     }

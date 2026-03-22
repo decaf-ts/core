b0fb6fb9f97d0d8dc658ae5822219d487245915f
tvenceslau <venceslau.tiago@gmail.com>
2026-03-22 08:58:27 +0000

    patch - DECAF-213 exposes TaskEventService ensuring compat with TskEngine-no-ci - after release preparation

--- src/tasks/TaskEventService.ts
+++ src/tasks/TaskEventService.ts
@@ -1,7 +1,8 @@
 import { Service } from "../services/services";
 import { type Repo } from "../repository/Repository";
 import { ModelService } from "../services/ModelService";
-import { TaskEventModel } from "./models/index";
+import { TaskEventModel } from "./models/TaskEventModel";
+import { TaskModel } from "./models/TaskModel";
 import { TaskService } from "./TaskService";
 
 export class TaskEventService extends ModelService<TaskEventModel> {
@@ -11,9 +12,9 @@ export class TaskEventService extends ModelService<TaskEventModel> {
 
   override get repo(): Repo<TaskEventModel> {
     if (!this._repository) {
-      this._repository = (
-        Service.get(TaskEventModel as any) as TaskService<any>
-      )["events"];
+      this._repository = (Service.get(TaskModel as any) as TaskService<any>)[
+        "events"
+      ];
     }
     return this._repository;
   }

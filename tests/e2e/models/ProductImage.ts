37f6e1c2b31dba1dcc5e43052c87bb1300d33cc1
dcardosoPdmfc <diogo.cardoso@pdmfc.com>
2026-04-24 15:40:47 +0100

    fix tests

--- tests/e2e/models/ProductImage.ts
+++ tests/e2e/models/ProductImage.ts
@@ -1,10 +1,11 @@
 import { description, uses } from "@decaf-ts/decoration";
-import { column, pk, table } from "@decaf-ts/core";
+
 import { model, type ModelArg, required } from "@decaf-ts/decorator-validation";
 import { E2eConfig } from "../e2e.config";
 import { TableNames } from "./constants";
 import { BaseIdentifiedModel } from "./BaseIdentifiedModel";
 import { gtin } from "./gtin";
+import { column, pk, table } from "../../../src";
 
 @description("Links a product to a specific market.")
 @uses(E2eConfig.flavour)
